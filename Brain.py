84449d721747a0f1936cf4c7f751a7d592d52953
NotEnoughMilk <31776017+NotEnoughMilk@users.noreply.github.com>
2019-11-11 09:37:29 -0500

    I added so much stuff :)

--- Brain.py
+++ Brain.py
@@ -1,58 +1,54 @@
-import Follow
-import UnFollow
-import TwitterBot
-from Follow import check_time
-from time import time
-from datetime import timedelta
+from Bot import TwitterBot
+import cv2
+import pyautogui
+import numpy as np
+from time import sleep
 
+
+def swap_user(user):
+    loc = locate_image('morebtn', .8)
+    for npt in zip(*loc[::-1]):
+        pyautogui.moveTo(npt[0] + 10, npt[1] + 20)
+        pyautogui.click()
+        sleep(.2)
+        pyautogui.click()
+        break
+    sleep(.3)
+    loc = locate_image('logoutbtn', .8)
+    for npt in zip(*loc[::-1]):
+        pyautogui.moveTo(npt[0] + 30, npt[1] + 20)
+        pyautogui.click()
+        break
+    sleep(2)
+    loc = locate_image('usernameentry', .8)
+    for npt in zip(*loc[::-1]):
+        pyautogui.moveTo(npt[0] + 30, npt[1] + 20)
+        pyautogui.click()
+        pyautogui.typewrite(user.credentials[0])
+
+        sleep(1)
+        pyautogui.moveRel(250)
+        pyautogui.click()
+        pyautogui.typewrite(user.credentials[1])
+        pyautogui.hotkey('enter')
+        sleep(1)
+        break
+
+
+def locate_image(imgname, threshold):
+    pyautogui.screenshot('img/screenshot.png')
+    img_rgb = cv2.imread('img/screenshot.png')
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('img/{}.png'.format(imgname), 0)
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)
+    return loc
+
+
+# Write stuff for the bot to do here
+# (follow, unfollow, swapuser)
 if __name__ == '__main__':
-    while True:
-        n = int(input('What would you like me to do? \n   1. Follow Users \n   2. Unfollow Users'
-                      ' \n   3. Try Selenium\n   4. Check Timers\n 5. Quit \n'))
-
-        if n == 1:
-            if check_time('follow') is False:
-                print('\nToo many followed today, wait a while longer.')
-                f = open('latesttime.txt').read().splitlines()
-                f = f[0].split()
-                print('{} Remaining.\n\n'
-                      .format(str(timedelta(seconds=((float(f[2]) + 86400) - time())))))
-                continue
-            print("Okay, looking for users to follow.")
-            Follow.follow()
-            print("Finished following.")
-
-        elif n == 2:
-            if check_time('unfollow') is False:
-                print('\nToo many unfollowed today, wait a while longer.')
-                f = open('latesttime.txt').read().splitlines()
-                f = f[1].split()
-                print('{} Remaining.\n\n'
-                      .format(str(timedelta(seconds=((float(f[2]) + 86400) - time())))))
-                continue
-            print("Okay, looking for users to unfollow.")
-            UnFollow.unfollow()
-            print("Finished unfollowing.")
-        elif n == 3:
-            print('This stuff is DEFINITELY still a WIP, so dont get too mad.')
-            TwitterRobot = TwitterBot.TwitterBoot()
-            TwitterRobot.searchKeyword()
-        elif n == 4:
-            f = open('latesttime.txt').read().splitlines()
-            f = f[0].split()
-            print('{} Remaining To Follow Again.'
-                  .format(str(timedelta(seconds=((float(f[2]) + 86400) - time())))))
-
-            f = open('latesttime.txt').read().splitlines()
-            f = f[1].split()
-            print('{} Remaining To Unfollow Again.\n\n'
-                  .format(str(timedelta(seconds=((float(f[2]) + 86400) - time())))))
-        elif n == 5:
-            print('Aw, okay. Bye!')
-            quit()
-        else:
-            print('I am not sure what that means')
-
-
+    bot1 = TwitterBot(user=0)
+    bot1.follow()
 
 
